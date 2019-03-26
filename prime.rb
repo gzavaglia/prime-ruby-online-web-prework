08dd56668a8bce7f40a14e8302ffd2facc1c69c7
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-26 19:56:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,20 +4,36 @@ if num <= 0 || num ==1
   return false
 end 
 len = div.size 
-counter = 0 
-until counter == len 
-key = div[counter]
-rest = num%key 
-if rest == 0 
-  return false
-else 
-  counter +=1
-  rest2 = num%key
-  if rest2 == 0 
-    return false
+counter = 0
+while counter <= len 
+tst(num,div[counter])
+end 
+end 
+# until counter == len 
+# key = div[counter]
+# rest = num%key 
+# if rest == 0 
+#   return false
+# else 
+#   counter +=1
+#   rest2 = num%key
+#   if rest2 == 0 
+#     return false
+#   else 
+#   return true
+# end 
+# end
+# end
+# end 
+
+def tst(num1,num2)
+  numx = num1%num2
+  if numx == 0
+    return false 
   else 
-  return true
+    return true 
+  end
 end 
-end
-end
-end 
+
+  
+  
