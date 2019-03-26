50ade3c00d3c7ad42938673b1057a2fe399ccf67
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-26 20:10:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -14,31 +14,7 @@ else
 end #end if2 
 end #end method  
 
-# until counter == len 
-# key = div[counter]
-# rest = num%key 
-# if rest == 0 
-#   return false
-# else 
-#   counter +=1
-#   rest2 = num%key
-#   if rest2 == 0 
-#     return false
-#   else 
-#   return true
-# end 
-# end
-# end
-# end 
 
-# def tst(num1,num2)
-#   numx = num1 % num2
-#   if numx == 0
-#     return false 
-#   else 
-#     return true 
-#   end
-# end 
 
   
   
