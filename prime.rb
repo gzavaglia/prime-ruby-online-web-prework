dadfa7832ed7275d995facb6d9792a60c9bfb934
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-26 19:48:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,23 @@
 def prime?(num)
-
+array = [2..num-1]
+if num <= 0 || num ==1 
+  return false
+end 
+len = div.size 
+counter = 0 
+until counter == len 
+key = div[counter]
+rest = num/key 
+if rest == 0 
+  return false
+else 
+  counter +=1
+  rest2 = num/key
+  if rest2 == 0 
+    return false
+  else 
+  return true
+end 
+end
+end
+end 
